efb63836da4b7a10adb916e5cf51a97f41d7f81e
fxhxyz4 <fxhxyz8@gmail.com>
2026-09-17 21:23:30 +0300

    chore: update info

---
+++ config/cfg.py
@@ -0,0 +1,41 @@
+"""
+Project configuration.
+
+Edit this file to customize the generated GitHub profile terminal.
+"""
+
+# Identity
+USER = "fxhxyz"
+HOSTNAME = "arch"
+GITHUB_USER = "fxhxyz"
+
+# Personal data used by the age counter
+BIRTH_DAY = 14
+BIRTH_MONTH = 2
+BIRTH_YEAR = 2006
+
+# Operating system
+DISTRO_NAME = "Arch Linux"
+KERNEL = "7.1.2-arch3-1"
+SHELL = "zsh 5.9.1"
+DE_WM = "Hyprland"
+TERMINAL_APP = "Kitty"
+EDITOR = "Neovim"
+
+# Hardware
+HOST_MACHINE = "Lenovo ThinkPad e15"
+CPU = "11th Gen Intel i5-1135G7"
+GPU = "Intel TigerLake-LP GT2"
+MEMORY = "16 GiB"
+
+# Links
+TELEGRAM_URL = "t.me/Lilya4everYoung"
+GITHUB_URL = "github.com/fxhxyz4"
+
+# Renderer
+TERMINAL_WIDTH = 860
+TERMINAL_HEIGHT = 480
+FPS = 15
+
+FONT_MAIN = "./fonts/gohufont-uni-14.ttf"
+FONT_SIZE = 15
